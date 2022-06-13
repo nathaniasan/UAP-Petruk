1ab41437bad170327f182a5aeb809937f0b37dcc
Tania <89388944+nathaniasan@users.noreply.github.com>
2022-06-13 21:51:10 +0700

    Update draf.cpp

--- draf.cpp
+++ draf.cpp
@@ -1,1 +1,53 @@
 //ini adalah draf projek uap
+#include<iostream>
+#include<algorithm>
+#include<conio.h>
+#include<vector>
+#include<stdlib.h>
+#include<iomanip>
+#include<string>
+
+#define max 50
+
+vector<int> v;
+typedef struct {
+    string name[max], nik[max], kelas[max];
+    int head, tail;
+}Queue;
+Queue antrian;
+
+void create(){
+    antrian.head=antrian.tail=-1;
+}
+
+bool isempty(){
+    if(antrian.tail==-1)
+        return true;
+    else
+        return false;
+}
+
+bool isfull(){
+    if (antrian.tail==max-1) 
+        return true;
+    else
+        return false;
+}
+
+int enqueue (string nama, string nik, string kelas){
+
+    if (isempty()==true)
+    {
+        antrian.head = antrian.tail = 0;
+    }
+    
+
+
+}
+using namespace std ;
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
